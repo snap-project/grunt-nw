b2515b93c68fc8942e45f0dd062b623c3dd42f51
William PETIT <william.petit@ac-dijon.fr>
2014-04-16 09:03:40 +0200

    Update node-webkit download URL

--- tasks/lib/defaults.js
+++ tasks/lib/defaults.js
@@ -1,5 +1,5 @@
 module.exports = {
   RUNTIME_VERSION: '0.9.2',
   DOWNLOAD_DIR: 'node-webkit',
-  DOWNLOAD_URL: 'https://s3.amazonaws.com/node-webkit'
+  DOWNLOAD_URL: 'http://dl.node-webkit.org'
 };
